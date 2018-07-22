c08a8d16e531c1f4922f8af778954ff11c8e4cf6
Victória Braia <victoriabraia@gmail.com>
2018-07-22 20:36:59 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Lucro
+{
+    class Program
+    {
+        public static int n, days, costPerDay, revenue, resp;
+        public static string line;
+        public static int[] profitPerDay = new int[50];
+        public static int[] aux = new int[50];
+
+        public static void CalcMaxProfit()
+        {
+            aux[0] = Math.Max(0, profitPerDay[0]);
+
+            for (int i = 1; i < days; i++)
+            {
+                aux[i] = Math.Max(profitPerDay[i] + aux[i - 1], Math.Max(0, profitPerDay[i]));
+            }
+            resp = aux.Max();
+        }
+
+        static void Main(string[] args)
+        {
+            while ((line = Console.ReadLine()) != null)
+            {
+                days = int.Parse(line);
+                costPerDay = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i < days; i++)
+                {
+                    revenue = int.Parse(Console.ReadLine());
+                    profitPerDay[i] = revenue - costPerDay;
+                }
+
+                CalcMaxProfit();
+                Console.WriteLine(resp);
+                Array.Clear(profitPerDay, 0, profitPerDay.Length);
+                Array.Clear(aux, 0, aux.Length);
+            }
+        }
+    }
+}
